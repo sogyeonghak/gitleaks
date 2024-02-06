1d122666ac08ddb1b371393ab05fe07281eef9ff
sogyeonghak <sogyeonghak@github.com>
2024-02-06 09:24:10 +0900

    commit_1

---
+++ __init__.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import yaml
+# jinja2 (thymeleaf)
+# flask 내에 자동으로 설치가 됨!
+
+app = Flask(__name__)
+
+# YAML 파일에서 설정 값을 읽어오기
+with open('config.yml') as file:
+    config = yaml.safe_load(file)
+
+# 읽어온 설정 값을 앱에 적용하기
+app.config.update(config)
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html', test=app.config.get('name')['gyeonghak'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
